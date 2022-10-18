852a011fc1d2504210a34857c3eb9e229c2b9069
joncoronel <59675970+joncoronel@users.noreply.github.com>
2022-10-18 13:39:41 -0700

    testing without landing page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.scss";
-import ContactUs from "../components/MainContent/ContactUs";
 import LandingPage from "../components/MainContent/LandingPage";
 import Contact from "../components/MainContent/Contact";
 import AboutUs from "../components/MainContent/AboutUs";
@@ -20,8 +19,6 @@ const Home = (props) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className={styles.spacing}>
-        <Hero />
-       
         <AboutUs />
         <Gallery names={value} folders={value} />
         <Contact />
