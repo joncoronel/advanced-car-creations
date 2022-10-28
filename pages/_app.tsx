6181c791444f9bd9779985f2845bd97495444095
joncoronel <59675970+joncoronel@users.noreply.github.com>
2022-10-28 05:40:48 -0700

    added analytics from vercel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import Layout from "../components/layout/layout";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+      <Analytics />
+    </>
   );
 }
 
