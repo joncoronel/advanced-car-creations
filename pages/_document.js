b9b6e94c8772addc744b14f8ad55fcd2c5c2f13f
joncoronel <59675970+joncoronel@users.noreply.github.com>
2022-11-01 07:35:21 -0700

    added local font

--- pages/_document.js
+++ pages/_document.js
@@ -3,14 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-
-        <link
-          href="https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Poppins:wght@300;400;500;600;700;800&display=swap"
-          rel="stylesheet"
-        ></link>
-      </Head>
+      <Head></Head>
       <body>
         <Main />
         <NextScript />
