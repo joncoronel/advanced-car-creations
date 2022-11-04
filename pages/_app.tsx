9c45deb1d27cb16be8caf9b48d153eefb98b01ee
joncoronel <59675970+joncoronel@users.noreply.github.com>
2022-11-03 23:40:28 -0700

    switched to swap local fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import Layout from "../components/layout/layout";
 import { Analytics } from "@vercel/analytics/react";
+import "../public/fonts.css"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
--- pages/_document.js
+++ pages/_document.js
@@ -4,22 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
-
-        <link
-          rel="preload"
-          as="style"
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap"
-          rel="stylesheet"
-        />
+        
       </Head>
       <body>
         <Main />
