2a1c654266b2d1f2e3a32448e1d7aec2a9fd8efe
joncoronel <59675970+joncoronel@users.noreply.github.com>
2022-11-19 22:43:46 -0800

    added hours to footer

--- components/layout/NewFooter.js
+++ components/layout/NewFooter.js
@@ -1,32 +1,67 @@
 import classes from "./NewFooter.module.scss";
 import { FaFacebook, FaInstagram, FaYelp } from "react-icons/fa";
 import Link from "next/link";
+import { useState, useEffect, useRef, useContext } from "react";
+
+import { ScrollContext } from "../layout/layout";
 
 export default function NewFooter() {
+  const section = useContext(ScrollContext);
+  const handleClick = (e) => {
+    e.current?.scrollIntoView({ behavior: "smooth" });
+  };
   return (
     <footer className={classes.footer}>
       <div className={classes.container}>
         <div className={classes.findUs}>
-          <div className={classes.title}>Find Us</div>
-          <div className={classes.content}></div>
+          <div className={classes.title}>Store Info</div>
+          <div className={classes.content}>
+            <div className={classes.address}>
+              13231 W Garden Grove Blvd<br></br>Garden Grove, CA <br></br>92843
+            </div>
+            <div className={classes.hours}>
+              Mon - Fri | 10am - 7pm <br></br>Sun | Closed
+            </div>
+          </div>
         </div>
         <div className={classes.quickLinks}>
           <div className={classes.title}>Quick Links</div>
           <div className={classes.content}>
             <ul className={classes.links}>
-              <li>Home</li>
-              <li>About Us</li>
-              <li>Gallery</li>
-              <li>Contact Us</li>
+              <li
+                onClick={() => {
+                  handleClick(section.hero);
+                }}
+              >
+                Home
+              </li>
+              <li
+                onClick={() => {
+                  handleClick(section.about);
+                }}
+              >
+                About Us
+              </li>
+              <li
+                onClick={() => {
+                  handleClick(section.gallery);
+                }}
+              >
+                Gallery
+              </li>
+              <li
+                onClick={() => {
+                  handleClick(section.contact);
+                }}
+              >
+                Contact Us
+              </li>
             </ul>
           </div>
         </div>
         <div className={classes.other}>
-          <div className={classes.title}>Find Us</div>
+          <div className={classes.title}>Socials</div>
           <div className={classes.content}>
-            <div className={classes.address}>
-              13231 W Garden Grove Blvd<br></br>Garden Grove, CA <br></br>92843
-            </div>
             <ul className={classes.socialMedia}>
               <li>
                 <a
